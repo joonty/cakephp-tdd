c6890b882fe0af2343585aef242c487db5a9c30f
Jonathan Cairns <jon@joncairns.com>
2012-03-10 16:30:38 +0000

    Added more generator methods

--- Lib/ValidationDataGenerator.php
+++ Lib/ValidationDataGenerator.php
@@ -6,97 +6,173 @@
  * @author Jon Cairns <jon@joncairns.com>
  */
 class ValidationDataGenerator {
+
 	public function getData($rule) {
 		if (is_string($rule)) {
-			$method = '_'.strtolower($rule);
+			$method = '_' . strtolower($rule);
+			$rule = array($rule);
 		} else if (is_array($rule)) {
-			$method = '_'.$rule[0];
+			$method = '_' . $rule[0];
 		} else {
 			throw new Exception("Invalid data type for rule");
 		}
-		
+
 		if (method_exists($this, $method)) {
 			return $this->$method($rule);
 		} else {
 			return false;
 		}
 	}
-	
+
 	protected function _numeric($rule) {
 		return rand();
 	}
-	
+
 	protected function _maxLength($rule) {
-		if (!$this->ensureArray('maxLength',$rule,2)) {
+		if (!$this->ensureArray('maxLength', $rule, 2)) {
 			return false;
 		}
 		return $this->lipsum($rule[1]);
 	}
-	
+
 	protected function _minLength($rule) {
-		if (!$this->ensureArray('minLength',$rule,2)) {
+		if (!$this->ensureArray('minLength', $rule, 2)) {
 			return false;
 		}
-		
+
 		$string = $this->lipsum($rule[1]);
-		while(strlen($string) < $rule[1]) {
-			$string .= ' '.$string;
+		while (strlen($string) < $rule[1]) {
+			$string .= ' ' . $string;
 		}
 		return $string;
 	}
-	
+
 	protected function _ip($rule) {
 		$parts = 4;
-		if (is_array($rule)) {
-			if (strcasecmp($rule[1],'ipv6')==0) {
+		if (count($rule) > 1) {
+			if (strcasecmp($rule[1], 'ipv6') == 0) {
 				$parts = 6;
 			}
 		}
 		$ip = '';
-		for ($i=0; $i < $parts -1; $i++) {
-			$ip.=rand(0,255).'.';
+		for ($i = 0; $i < $parts - 1; $i++) {
+			$ip.=rand(0, 255) . '.';
 		}
-		return $ip.rand(0,255);
+		return $ip . rand(0, 255);
 	}
-	
-	protected function email() {
-		$ends = array('com','co.uk','org','net','org.uk','biz','me');
+
+	protected function _email() {
+		$ends = array('com', 'co.uk', 'org', 'net', 'org.uk', 'biz', 'me');
 		$words = $this->lipsumWords(2);
-		return $words[0].'@'.$words[1].'.'.array_rand($ends);
+		return $words[0] . '@' . $words[1] . '.' . $ends[array_rand($ends)];
 	}
 	
-	protected function ensureArray($ruleName,$data,$length = null) {
+	protected function _boolean() {
+		return true;
+	}
+
+	protected function _date($rule) {
+		$formats = $this->dateFormatMap();
+		//Convert custom cake formats to PHP date() formats
+		if (!isset($rule[1]) || !array_key_exists($rule[1],$formats)) {
+			$rule[1] = 'ymd';
+		}
+		return date($formats[$rule[1]]);
+	}
+
+	protected function _datetime($rule) {
+		$formats = $this->dateFormatMap();
+		//Convert custom cake formats to PHP date() formats
+		if (!isset($rule[1]) || !array_key_exists($rule[1],$formats)) {
+			$rule[1] = 'ymd';
+		}
+		return date($formats[$rule[1]].' H:i');
+	}
+	
+	protected function dateFormatMap() {
+		$format = array();
+		$format['dmy'] = 'd-m-Y';
+		$format['mdy'] = 'm-d-Y';
+		$format['ymd'] = 'Y-m-d';
+		$format['dMy'] = 'd F Y';
+		$format['Mdy'] = 'F d Y';
+		$format['My'] = 'F Y';
+		$format['my'] = 'm/Y';
+		return $format;
+	}
+	
+	protected function _alphanumeric() {
+		$characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		$string = '';
+		for ($i = 0; $i < 10; $i++) {
+			$string .= $characters[rand(0, strlen($characters) - 1)];
+		}
+		return $string;
+	}
+	
+	protected function _decimal() {
+		return (float)rand(0,100)/100;
+	}
+	
+	protected function _extension($rule) {
+		if (count($rule) < 2 || !is_array($rule[1])) {
+			$extensions = array('gif', 'jpeg', 'png', 'jpg');
+		} else {
+			$extensions = $rule[1];
+		}
+		return $this->lipsumWords(1).'.'.  $extensions[array_rand($extensions)];
+	}
+	
+	protected function _equalTo($rule) {
+		if (!$this->ensureArray('equalTo',$rule,2)) {
+			return false;
+		}
+		return $rule[1];
+	}
+	
+	protected function _inList($rule) {
+		if (!$this->ensureArray('inList',$rule,2)) {
+			return false;
+		}
+		if (!is_array($rule[1])) {
+			trigger_error("Invalid rule definition for 'inList' (second parameter must be an array of options)");
+		}
+		return $rule[1][array_rand($rule[1])];
+	}
+
+	protected function ensureArray($ruleName, $data, $length = null) {
 		if (!is_array($data)) {
-			trigger_error("Invalid rule definition for $ruleName (rule must be an array)",E_USER_WARNING);
+			trigger_error("Invalid rule definition for $ruleName (rule must be an array)", E_USER_WARNING);
 			return false;
 		}
 		if (!is_null($length) && count($data) < $length) {
-			trigger_error("Invalid rule definition for $ruleName (expected $length parameters)",E_USER_WARNING);
+			trigger_error("Invalid rule definition for $ruleName (expected $length parameters)", E_USER_WARNING);
 			return false;
 		}
 		return true;
 	}
-	
+
 	protected function lipsum($length = null) {
 		$lipsum = <<<EOD
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet, dui id malesuada tristique, nisl diam feugiat erat, a accumsan purus neque sit amet sapien. Aliquam gravida sollicitudin est, ultricies dapibus purus molestie ac. Pellentesque faucibus sem a enim accumsan posuere.
 EOD;
 		if (!is_null($length)) {
-			return substr($lipsum,0,$length);
+			return substr($lipsum, 0, $length);
 		} else {
 			return $lipsum;
 		}
 	}
-	
+
 	protected function lipsumWords($number) {
 		$lipsum = $this->lipsum();
-		$parts = explode(' ',$lipsum);
+		$parts = explode(' ', $lipsum);
 		$ret = array();
-		while(count($ret) < $number) {
-			$ret[] = str_replace('.','',array_rand($parts));
+		while (count($ret) < $number) {
+			$ret[] = str_replace(array('.', ','), '', $parts[array_rand($parts)]);
 		}
 		return $ret;
 	}
+
 }
 
 ?>
--- Test/Case/Lib/ValidationDataGeneratorTest.php
+++ Test/Case/Lib/ValidationDataGeneratorTest.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('ValidationDataGenerator','Tdd.Lib');
+App::uses('Validation','Utility');
 
 /**
  * Description of ValidationDataGeneratorTest
@@ -36,17 +37,114 @@ class ValidationDataGeneratorTestCase extends CakeTestCase {
 	
 	public function testGetDataWithIp() {
 		$data = $this->sut->getData('ip');
+		$this->assertInternalType('string',$data);
 		$this->assertRegExp('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/',$data);
 	}
 	
 	public function testGetDataWithIpv6() {
 		$data = $this->sut->getData(array('ip','IPV6'));
+		$this->assertInternalType('string',$data);
 		$this->assertRegExp('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/',$data);
 	}
 	
 	public function testGetDataWithEmail() {
-		$data = $this->sut->getData(array('ip','IPV6'));
-		$this->assertRegExp('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/',$data);
+		$data = $this->sut->getData('email');
+		$this->assertInternalType('string',$data);
+		$this->assertRegExp('/^[a-z0-9]+@[a-z0-9]+\.[a-z.]{2,6}$/i',$data,"String should be a valid email address");
+	}
+
+	public function testGetDataWithAlphaNumeric() {
+		$data = $this->sut->getData('alphanumeric');
+		$this->assertInternalType('string',$data);
+		$this->assertRegExp('/^[a-z0-9]+$/i',$data,"String should contain only alphanumeric characters");
+	}
+
+	public function testGetDataWithAlphaNumericCase() {
+		$data = $this->sut->getData('alphaNumeric');
+		$this->assertInternalType('string',$data);
+		$this->assertRegExp('/^[a-z0-9]+$/i',$data,"String should contain only alphanumeric characters");
+	}
+	
+	public function testGetDataWithBoolean() {
+		$data = $this->sut->getData('boolean');
+		$this->assertInternalType('boolean',$data);
+		$this->assertTrue($data);
+	}
+	
+	public function provideDateFormats() {
+		return array(
+			array('dmy'),
+			array('mdy'),
+			array('ymd'),
+			array('dMy'),
+			array('Mdy'),
+			array('My'),
+			array('my')
+		);
+	}
+
+	/**
+	 * @dataProvider provideDateFormats 
+	 */
+	public function testGetDataWithDate($format) {
+		$data = $this->sut->getData(array('date',$format));
+		$this->assertInternalType('string',$data);
+		
+		$this->assertTrue(Validation::date($data, $format),"Date failed validation");
+	}
+
+	public function testGetDataWithDateDefaultFormat() {
+		$data = $this->sut->getData('date');
+		$this->assertInternalType('string',$data);
+		
+		$this->assertTrue(Validation::date($data, 'ymd'),"Date failed validation");
+	}
+	
+
+	/**
+	 * @dataProvider provideDateFormats 
+	 */
+	public function testGetDataWithDatetime($format) {
+		$data = $this->sut->getData(array('datetime',$format));
+		$this->assertInternalType('string',$data);
+		
+		$this->assertTrue(Validation::datetime($data, $format),"Datetime failed validation");
+	}
+	
+	public function testGetDataWithDatetimeDefaultFormat() {
+		$data = $this->sut->getData('datetime');
+		$this->assertInternalType('string',$data);
+		
+		$this->assertTrue(Validation::datetime($data, 'ymd'),"Datetime failed validation");
+	}
+	
+	public function testGetDataWithDecimal() {
+		$data = $this->sut->getData('decimal');
+		$this->assertInternalType('float',$data);
+	}
+	
+	public function testGetDataWithExtension() {
+		$data = $this->sut->getData('extension');
+		$this->assertInternalType('string',$data);
+		$this->assertRegExp('~.+\.(gif|jpg|png|jpeg)$~',$data,"invalid extension returned");
+	}
+	
+	public function testGetDataWithCustomExtension() {
+		$data = $this->sut->getData(array('extension',array('pdf','zip')));
+		$this->assertInternalType('string',$data);
+		$this->assertRegExp('~.+\.(pdf|zip)$~',$data,"invalid extension returned");
+	}
+	
+	public function testGetDataWithEqualTo() {
+		$equalValue = "This must be the result";
+		$data = $this->sut->getData(array('equalTo',$equalValue));
+		$this->assertEqual($equalValue,$data,"Result must be equal");
+	}
+	
+	public function testGetDataWithInList() {
+		$equalList = array("This is a result",3,"Blah blah");
+		$data = $this->sut->getData(array('inList',$equalList));
+		$this->assertTrue(in_array($data,$equalList),"Result must be one of the supplied values");
 	}
 }
 
